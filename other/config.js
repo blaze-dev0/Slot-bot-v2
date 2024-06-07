edecef2dce0d421dae140b943dcdf117c9184126
Mafia yt <91059034+mafiayt69@users.noreply.github.com>
2024-06-07 08:19:24 +0530

    Update config.js

--- other/config.js
+++ other/config.js
@@ -8,6 +8,8 @@ const cf = {
       role_x: "slot role user id", //slot user role id
   
       staff_x: "staff role id", // staff role id
+
+      logchannel: '', //channel id for slot revoke notice
   
       slotrule: `set your slot rules`, // your slot rules
     }
